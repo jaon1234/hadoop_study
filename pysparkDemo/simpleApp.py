02d9c06a65e26d9c8601e7f8beced6a305c27e47
Jaon <jasonlmh0314@outlook.com>
2023-04-08 20:29:36 +0800

    spark RDDs笔记

--- pysparkDemo/simpleApp.py
+++ pysparkDemo/simpleApp.py
@@ -2,8 +2,8 @@
 from pyspark.sql import SparkSession
 import sys
 
-logFile = "/opt/modules/spark-3.2.3-bin-hadoop3.2/README.md"  # Should be some file on your system
-logFile = sys.argv[1]
+logFile = "E:\\hadoop_data\\input\\hello.txt"  # Should be some file on your system
+# logFile = sys.argv[1]
 spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
 logData = spark.read.text(logFile).cache()
 
--- pysparkDemo/sparkCon.py
+++ pysparkDemo/sparkCon.py
@@ -1,14 +1,21 @@
 from pyspark import SparkContext, SparkConf
 
-# 创建连接配置
-conf = SparkConf().setAppName('sparkRddDemo').setMaster("spark://172.21.128.1:7077")
+# # 创建连接配置
+# conf = SparkConf().setAppName('sparkRddDemo').setMaster("spark://sparkstandalone:7077")
+# # 在win主机连接standalone模式的集群时，指定driver的地址就是win主机的ip
+# conf.set("spark.driver.host","192.168.88.1")
+
+# 本地
+# 创建连接配置，本地连接
+conf = SparkConf().setAppName('sparkRddDemo').setMaster("local[2]")
 # 获取spark上下文,创建到集群的连接
 sc =  SparkContext(conf=conf)
 
 
-data = [1, 2, 3, 4, 5]
-distData = sc.parallelize(data)
-
-print(distData.collect())
+# 从文本文件创建
+distFile = sc.textFile("pysparkDemo\README.md")
+print(distFile)
+# 计算所有行的长度
+print(distFile.map(lambda s:len(s)).reduce(lambda a,b:a+b))
 
 sc.stop()
