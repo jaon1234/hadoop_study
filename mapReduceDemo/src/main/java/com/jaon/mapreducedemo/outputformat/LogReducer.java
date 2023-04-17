a51a43f0982b0eca3e94cbe29f5dd086424e9ebe
Jaon <jasonlmh0314@outlook.com>
2023-04-17 18:45:13 +0800

    xiugai

--- mapReduceDemo/src/main/java/com/jaon/mapreducedemo/outputformat/LogReducer.java
+++ mapReduceDemo/src/main/java/com/jaon/mapreducedemo/outputformat/LogReducer.java
@@ -5,13 +5,13 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.util.Iterator;
 
 public class LogReducer extends Reducer<Text, NullWritable,Text,NullWritable> {
     @Override
     protected void reduce(Text key, Iterable<NullWritable> values, Reducer<Text, NullWritable, Text, NullWritable>.Context context) throws IOException, InterruptedException {
         // 可能存在日志相同的情况，遍历values来输出可以避免丢数据
         for (NullWritable value : values) {
+            System.out.println(value);
             context.write(key,NullWritable.get());
         }
     }
