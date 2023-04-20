7285f12c54a5b0367c5f1c4afbc9c009a9ad482a
Jaon <jasonlmh0314@outlook.com>
2023-04-20 12:02:52 +0800

    linux环境配置

---
+++ 大数据的各类配置/系统环境配置/my_env.sh
@@ -0,0 +1,12 @@
+# java_home
+export JAVA_HOME=/opt/modules/jdk1.8.0_361
+export PATH=$PATH:$JAVA_HOME/bin
+
+# hadoop_home
+export HADOOP_HOME=/opt/modules/hadoop-3.2.4
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/sbin
+
+# hive
+export HIVE_HOME=/opt/modules/hive
+export PATH=$PATH:$HIVE_HOME/bin
